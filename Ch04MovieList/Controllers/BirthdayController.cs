672611d0df8511256fa63c8276a18b2e9caf018c
Anna <15157@LAPTOP-S74UCRP4>
2020-06-14 22:47:42 -0500

    Added age calculator and unit test project

---
+++ Ch04MovieList/Controllers/BirthdayController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ch04MovieList.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ch04MovieList.Controllers
+{
+    public class BirthdayController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Age = null;
+            ViewBag.Name = null;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(BirthDateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Age = model.CalculateCurrentAge();
+                ViewBag.Name = model.FirstName;
+            }
+            else
+            {
+                ViewBag.Age = null;
+                ViewBag.Name = null;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Ch04MovieList/Models/BirthDateModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ch04MovieList.Models
+{
+    public class BirthDateModel
+    {
+        [Required(ErrorMessage = "Please enter your name.")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter a birth date in the format of mm/dd/yyyy")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public string BirthDate { get; set; }
+        public int CalculateCurrentAge()
+        {
+            DateTime dateOfBirth = DateTime.Parse(BirthDate);
+            DateTime today = DateTime.Now;
+
+            TimeSpan difference = today - dateOfBirth;
+            int currentAge = (int)(difference.TotalDays / 365);
+            return currentAge;
+        }
+    }
+}
